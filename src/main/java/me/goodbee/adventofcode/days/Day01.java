5bf1d52f01f2da1babd1e235a2f93e4ac97b94c7
goodbee <good.bee12345@gmail.com>
2024-12-01 07:57:19 +0100

    day 1

--- src/main/java/me/goodbee/adventofcode/days/Day01.java
+++ src/main/java/me/goodbee/adventofcode/days/Day01.java
@@ -2,6 +2,9 @@ package me.goodbee.adventofcode.days;
 
 import me.goodbee.adventofcode.util.Day;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Day01 extends Day {
     @Override
     public String getInputFile() {
@@ -10,11 +13,64 @@ public class Day01 extends Day {
 
     @Override
     public String part1(String input) {
-        return input;
+        String[] pairs = input.split("\n");
+
+        int[] leftNums = new int[pairs.length];
+        int[] rightNums = new int[pairs.length];
+
+        for (int i = 0; i < pairs.length; i++) {
+            String[] arr = pairs[i].split(" {3}");
+
+            leftNums[i] = Integer.parseInt(arr[0]);
+            rightNums[i] = Integer.parseInt(arr[1]);
+        }
+
+        Arrays.sort(leftNums);
+        Arrays.sort(rightNums);
+
+        int totalDistance = 0;
+
+        for (int i = 0; i < pairs.length; i++) {
+            int leftNum = leftNums[i];
+            int rightNum = rightNums[i];
+
+            totalDistance += leftNum > rightNum ? leftNum - rightNum : rightNum - leftNum;
+        }
+
+        return Integer.toString(totalDistance);
     }
 
     @Override
     public String part2(String input) {
-        return "Hello " + input;
+        String[] pairs = input.split("\n");
+
+        int[] leftNums = new int[pairs.length];
+        int[] rightNums = new int[pairs.length];
+
+        for (int i = 0; i < pairs.length; i++) {
+            String[] arr = pairs[i].split(" {3}");
+
+            leftNums[i] = Integer.parseInt(arr[0]);
+            rightNums[i] = Integer.parseInt(arr[1]);
+        }
+
+        int similarityScore = 0;
+
+        for (int i = 0; i < pairs.length; i++) {
+            int leftnum = leftNums[i];
+            int multiply = 0;
+
+            for (int rightIndex = 0; rightIndex < pairs.length; rightIndex++) {
+                int rightNum = rightNums[rightIndex];
+
+                if(leftnum == rightNum) {
+                    multiply += 1;
+                }
+            }
+
+            similarityScore += leftnum * multiply;
+        }
+
+        return Integer.toString(similarityScore);
     }
 }
